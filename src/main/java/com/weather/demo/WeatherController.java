215d6863e6fed7ceb2c5e00f345204d294def912
vishwanath.rao <vishwanath.nagendra@gmail.com>
2021-02-23 15:53:00 +0530

    Commit 2 : Added Weather endpoint in controller to display basic JSON received from "https://samples.openweathermap.org/".

--- src/main/java/com/weather/demo/WeatherController.java
+++ src/main/java/com/weather/demo/WeatherController.java
@@ -2,6 +2,7 @@ package com.weather.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WeatherController {
@@ -9,5 +10,13 @@ public class WeatherController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello";
+    }
+
+    @RequestMapping("/weather")
+    public String weather(){
+        String weatherUrl = "https://samples.openweathermap.org/data/2.5/forecast?q=London%2Cus&appid=d2929e9483efc82c82c32ee7%20e02d563e";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(weatherUrl, String.class);
+        return result;
     }
 }
