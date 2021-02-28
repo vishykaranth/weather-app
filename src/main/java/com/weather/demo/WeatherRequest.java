86beca53f4a9b866dab626f17f7ce9d657cc6a94
vishykaranth <vishykaranth@gmail.com>
2021-02-28 18:19:40 +0530

    Initial Commit

--- src/main/java/com/weather/demo/WeatherRequest.java
+++ src/main/java/com/weather/demo/WeatherRequest.java
@@ -1,39 +1,24 @@
 package com.weather.demo;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.util.Date;
 
 public class WeatherRequest {
-    private Date date;
-    private int temp_min;
-    private int temp_max;
-    private String weatherCondition;
+    @Getter @Setter private Date date;
+    @Getter private int minTemp;
+    @Getter private int maxTemp;
+    @Getter private String weatherCondition;
 
     public WeatherRequest(Date date, int temp_min, int temp_max, String weatherCondition) {
 
         this.date = date;
-        this.temp_min = kelvinToCelsius(temp_min);
-        this.temp_max = kelvinToCelsius(temp_max);
+        this.minTemp = kelvinToCelsius(temp_min);
+        this.maxTemp = kelvinToCelsius(temp_max);
         this.weatherCondition = weatherCondition;
     }
 
     private int kelvinToCelsius(int temp) {
         return temp - 273;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public int getMinTemp() {
-        return temp_min;
-    }
-
-    public int getMaxTemp() {
-        return temp_max;
-    }
-
-    public String getWeatherCondition() {
-        return weatherCondition;
     }
 
     public int day() {
